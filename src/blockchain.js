4b570e98b4d3faa1cf4855f3d38c82b43e88baa2
YJC mac <yoon_je_chung@solomonsc.com>
2018-06-12 03:15:44 +0900

    created block constructor

---
+++ src/blockchain.js
@@ -0,0 +1,9 @@
+class Block {
+    constructor(index, hash, previoushHash, timestamp, data) {
+        this.index = index;
+        this.hash = hash;
+        this. previoushHash = previoushHash;
+        this.timestamp = timestamp;
+        this.data = data;
+    }
+}
