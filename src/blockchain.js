4bb1c05cbd82e8ab426a54b2b34405e5b1f64782
YJC mac <yoon_je_chung@solomonsc.com>
2018-06-12 03:25:15 +0900

    created first block

--- src/blockchain.js
+++ src/blockchain.js
@@ -7,3 +7,14 @@ class Block {
         this.data = data;
     }
 }
+
+const genesisBlock = new Block(
+  0,
+  "C48F80B36EC6881C21B6795928065967AD5DDC4898B3E89B044374FE6B78806E",
+  null,
+  1528741246228,
+  "This is genesis!!"
+);
+
+let blockchain = [genesisBlock];
+console.log(blockchain);
