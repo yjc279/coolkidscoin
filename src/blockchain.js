ebde08cb5dfe4d3a6e46fd16903195aef373a00d
YJC mac <yoon_je_chung@solomonsc.com>
2018-06-12 03:52:42 +0900

    created new block function

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,3 +1,4 @@
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previoushHash, timestamp, data) {
         this.index = index;
@@ -17,4 +18,30 @@ const genesisBlock = new Block(
 );
 
 let blockchain = [genesisBlock];
-console.log(blockchain);
+
+const getLastBlock = () => blockchain[blockchain.length -1];
+
+const getTimeStamp = () => new Date().getTime() / 1000;
+
+const createHash = (index, previousHash, timestamp, data) => 
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+const createNewBlock = data => {
+    const previousBlock = getLastBlock();
+    const newBlockIndex = previousBlock.index + 1;
+    const newTimeStamp = getTimeStamp();
+    const newHash = createHash(
+        newBlockIndex, 
+        previousBlock.hash, 
+        newTimeStamp, 
+        data);
+    const newBlock = new Block(
+        newBlockIndex,
+        newHash,
+        previousBlock.hash,
+        newTimeStamp,
+        data
+    );
+    return newBlock;
+}
+
