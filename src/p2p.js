9a70a9a7277be21ce01a67c637e7674a9dce9323
YJC mac <yoon_je_chung@solomonsc.com>
2018-06-24 02:39:05 +0900

    sending messages between peers

--- src/p2p.js
+++ src/p2p.js
@@ -1,6 +1,36 @@
-const WebSockets = require('ws');
+const WebSockets = require('ws'),
+    BlockChain  = require("./blockchain");
+
+const { getLastBlock } = BlockChain;
 
 const sockets = [];
+
+//Message TYPES
+const GET_LATEST = "GET_LATEST";
+const GET_ALL = "GET_ALL";
+const BLOCKCHAIN_RESPONSE = "BLOCKCHAIN_RESPONSE";
+
+//MESSAGE CREATORS
+const getLatest = () => {
+    return {
+        type: GET_LATEST,
+        data: null
+    };
+};
+
+const getALL = () => {
+    return {
+        type: GET_ALL,
+        data: null
+    };
+};
+
+const blockchainResponse = data => {
+    return {
+        type: BLOCKCHAIN_RESPONSE,
+        data
+    };
+};
 
 const getSockets = () => sockets;
 
@@ -12,18 +42,39 @@ const startP2PServer = server => {
     console.log("Cool Kids Coin P2P Server Running!");
 }
 
-const initSocketConnection = socket => {
-    sockets.push(socket);
-    handleSocketError(socket);
-    socket.on("message", (data) => {
-        console.log(data);
-    });
-    setTimeout(() => {
-       socket.send("welcome");
-    }, 5000);
+const initSocketConnection = ws => {
+    sockets.push(ws);
+    handleSocketMessages(ws);
+    handleSocketError(ws);
+    sendMessage(ws, getLatest());
 };
 
+const parseData = data => {
+ try{
+    return JSON.parse(data);
+ } catch(e) {
+     console.log(e);
+     return null;
+ }
+};
 
+const handleSocketMessages = ws => {
+    ws.on("message", data => {
+        const message = parseData(data);
+        if(message === null){
+            return;
+        }
+        console.log(message);
+        switch(message.type){
+            case GET_LATEST: 
+                sendMessage(ws, getLastBlock());
+                break;
+            
+        }
+    });
+};
+
+const sendMessage = (ws, message) => ws.send(JSON.stringify(message));
 
 const handleSocketError = ws => {
     const closeSocketConnection = ws => {
