784e681a09d915a9a92c054338e1b7e2471bbc87
YJC mac <yoon_je_chung@solomonsc.com>
2018-06-29 02:36:48 +0900

    defining transaction classes

---
+++ src/transactions.js
@@ -0,0 +1,42 @@
+const CryptoJS = require("crypto-js");
+
+class TxOut{
+    constructor(address,amount){
+        this.address = address;
+        this.amount = amount;
+    }
+}
+
+class TxIn{
+    //uTxOutId
+    //uTxOutIndex
+    // Signature
+}
+
+class Transaction{
+    //ID
+    //txIns[]
+    //txOuts[]
+}
+
+class UTxOut{
+    constructor(uTxOutId, uTxOutIndex, address, amount){
+        this.uTxOutId = uTxOutId;
+        this.uTxOutIndex = uTxOutIndex;
+        this.address = address;
+        this.amount = amount;
+    }
+}
+
+let uTxOuts = [];
+
+const getTxId = tx => {
+    const txInContent = tx.txIns
+        .map(txIn => txIn.uTxOutId + txIn.uTxOutIndex)
+        .reduce((a,b) => a + b,"");
+
+    const txOutContent = tx.txOuts
+        .map(txOut => txOut.address + txOut.amount)
+        .reduce((a,b) => a + b, "");
+    return CryptoJS.SHA256(txInContent + txOutContent).toString();
+};
