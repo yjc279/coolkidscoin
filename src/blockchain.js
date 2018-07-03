cc8203264ddb221f4b38ede528e597565c94fce4
YJC mac <yoon_je_chung@solomonsc.com>
2018-07-04 04:50:22 +0900

    processing coinbase transaction

--- src/blockchain.js
+++ src/blockchain.js
@@ -4,7 +4,7 @@ const CryptoJS = require("crypto-js"),
     hexToBinary = require("hex-to-binary");
 
 const { getBalance, getPublicFromWallet } = Wallet;
-const { createCoinbaseTx } = Transactions;
+const { createCoinbaseTx, processTxs } = Transactions;
 
 const BLOCK_GENERATION_INTERVAL = 10;
 const DIFFICULTY_ADJUSTMENT_INTERVAL = 10;
@@ -217,7 +217,18 @@ const replaceChain = candidateChain => {
 
 const addBlockToChain = candidateBlock => {
     if (isBlockValid(candidateBlock, getNewestBlock())) {
-        blockchain.push(candidateBlock);
+        const processedTxs = processTxs(
+            candidateBlock.data,
+            uTxOuts,
+            candidateBlock.index
+        );
+        if(processedTxs === null){
+            console.log("Couldn't process txs");
+            return false;
+        } else {
+            blockchain.push(candidateBlock);
+            uTxOuts = processedTxs;
+        }  
         return true;
     } else {
         return false;
